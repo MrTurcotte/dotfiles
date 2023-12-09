8bf002169829a541f4d64030f7dadc11a5441817
David Turcotte <git@davidturcotte.com>
2023-12-09 01:11:46 -0500

    update Sat 09 Dec 2023 01:11:46 AM EST

--- nixos/packages/default.nix
+++ nixos/packages/default.nix
@@ -8,7 +8,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-#    nixos-generators
+    nixos-generators
 #    protonvpn-gui
     psutils
     psmisc
