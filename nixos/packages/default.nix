91cccb178d5091dd223ef4f85d85dceb38f6dccd
David Turcotte <git@davidturcotte.com>
2023-12-08 19:49:41 -0500

    update Fri 08 Dec 2023 07:49:41 PM EST

--- nixos/packages/default.nix
+++ nixos/packages/default.nix
@@ -8,89 +8,90 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    protonvpn-gui
     psutils
     psmisc
     python3
-    pkgs.pywal
-    pkgs.wofi
-    pkgs.fuse
-    pkgs.speedcrunch
-    pkgs.suricata
-    pkgs.tcpdump
-    #    pkgs.busybox
-    pkgs.xss-lock
-    pkgs.libnotify
-    pkgs.libsForQt5.ghostwriter
-    pkgs.alacritty
-    #    pkgs.android-studio
-    #    pkgs.jetbrains.idea-community
-    pkgs.appimage-run
-    pkgs.bintools
-    pkgs.binutils
-    pkgs.brave
-    pkgs.cifs-utils
-    pkgs.deluge
-    #    pkgs.devour
-    pkgs.dmidecode
-    pkgs.evince
-    pkgs.eza
-    #    pkgs.gcc
-    pkgs.gimp-with-plugins
-    pkgs.gitFull
-    pkgs.google-chrome
-    pkgs.gparted
-    pkgs.gst_all_1.gst-libav
-    pkgs.htop
-    pkgs.imagemagick
-    #    pkgs.inkscape-with-extensions
-    pkgs.kotlin
-    #    pkgs.krita
-    pkgs.libreoffice-qt
-    pkgs.libsecret
-    #    pkgs.libsForQt5.kdenlive
-    pkgs.libvorbis
-    pkgs.lutris
-    pkgs.mgba
-    pkgs.mpv
-    pkgs.mutt
-    pkgs.nasm
-    pkgs.neofetch
-    pkgs.newsboat
-    pkgs.nixfmt
-    pkgs.nixos-generators
-    pkgs.nuclear
-    #    pkgs.obs-studio
-    pkgs.openjdk11
-    pkgs.patchelf
-    pkgs.polkit_gnome
-    pkgs.pywal
-    #    pkgs.rclone
-    #    pkgs.ripgrep-all
-    pkgs.snes9x
-    pkgs.snes9x-gtk
-    pkgs.spotify
-    #    pkgs.st
-    #    pkgs.steam
-    #    pkgs.sublime
-    pkgs.sxiv
-    pkgs.texlive.combined.scheme-full
-    pkgs.texstudio
-    pkgs.transmission
-    pkgs.unzip
-    pkgs.vim
-    pkgs.vlc
-    pkgs.vorbis-tools
-    pkgs.vorbisgain
-    pkgs.vscode-with-extensions
-    pkgs.wget
-    pkgs.winetricks
-    #    pkgs.wineWowPackages.waylandFull
-    pkgs.wineWowPackages.stable
-    pkgs.woeusb
-    pkgs.wordgrinder
-    #    pkgs.wpsoffice
-    pkgs.xautolock
-    pkgs.youtube-dl
+    pywal
+    wofi
+    fuse
+    speedcrunch
+    suricata
+    tcpdump
+    #    busybox
+    xss-lock
+    libnotify
+    libsForQt5.ghostwriter
+    alacritty
+    #    android-studio
+    #    jetbrains.idea-community
+    appimage-run
+    bintools
+    binutils
+    brave
+    cifs-utils
+    deluge
+    #    devour
+    dmidecode
+    evince
+    eza
+    #    gcc
+    gimp-with-plugins
+    gitFull
+    google-chrome
+    gparted
+    gst_all_1.gst-libav
+    htop
+    imagemagick
+    #    inkscape-with-extensions
+    kotlin
+    #    krita
+    libreoffice-qt
+    libsecret
+    #    libsForQt5.kdenlive
+    libvorbis
+    lutris
+    mgba
+    mpv
+    mutt
+    nasm
+    neofetch
+    newsboat
+    nixfmt
+    nixos-generators
+    nuclear
+    #    obs-studio
+    openjdk11
+    patchelf
+    polkit_gnome
+    pywal
+    #    rclone
+    #    ripgrep-all
+    snes9x
+    snes9x-gtk
+    spotify
+    #    st
+    #    steam
+    #    sublime
+    sxiv
+    texlive.combined.scheme-full
+    texstudio
+    transmission
+    unzip
+    vim
+    vlc
+    vorbis-tools
+    vorbisgain
+    vscode-with-extensions
+    wget
+    winetricks
+    #    wineWowPackages.waylandFull
+    wineWowPackages.stable
+    woeusb
+    wordgrinder
+    #    wpsoffice
+    xautolock
+    youtube-dl
   ];
 
   programs = {
