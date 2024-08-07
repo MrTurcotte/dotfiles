4e943a561201ca2ca8b830f817d2b7b2d149c8ad
David Turcotte <d.turcotte@gmail.com>
2024-08-07 01:16:43 -0400

    update Wed 07 Aug 2024 01:16:43 AM EDT

---
+++ nixos/oldConfig/home/homeManager/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }: {
+  imports = [ <home-manager/nixos> ];
+  home-manager.users.dave = {
+    # The home.stateVersion option does not have a default and must be set
+    home.stateVersion = "23.05";
+    # Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
+    #    programs.bash = {
+    #      enable = true;
+    #      bashrcExtra = ''
+    #        . ./bashrc
+    #      '';
+    #    };
+    #    home.packages = with pkgs; [ home-manager ];
+    home.file.".bashrc".source = ./bashrc;
+    #    programs.home-manager.enable = true;
+
+  };
+}
